54ff7d0e8081e343c37ceef99cc02b5b8ab1d446
Yuristam <yuristamasanakunov@gmail.com>
2025-06-09 13:49:04 +0600

    Edited: Publishers

--- GamesDB/Tables/Publishers.sql
+++ GamesDB/Tables/Publishers.sql
@@ -3,3 +3,13 @@ CREATE TABLE Publishers (
     PublisherName NVARCHAR(100) NOT NULL,
     Description NVARCHAR(500) NULL
 )
+
+ALTER TABLE Publishers 
+    ADD CountryID INT NOT NULL
+
+ALTER TABLE Publishers 
+	ADD CONSTRAINT UQ_Publisher_Name UNIQUE (PublisherName)
+
+ALTER TABLE Publishers 
+	ADD CONSTRAINT FK_Publishers_Countries FOREIGN KEY (CountryID) 
+	REFERENCES Countries (CountryID)
